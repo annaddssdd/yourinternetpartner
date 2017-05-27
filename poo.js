e043adb426c4fe0ead672d6b6d4e7dd16ed1f628
annaddssdd <annaddssdd@gmail.com>
2017-05-27 14:03:23 +1200

    Update poo.js

--- poo.js
+++ poo.js
@@ -20,11 +20,18 @@ function onClick() {
 }
 
 function onSuccess(googleUser) {
-		console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+		var profile = googleUser.getBasicProfile();
+		console.log('Logged in as: ' + profile.getName());
+		console.log('ID: ' + profile.getId());
+		console.log('Email: ' + profile.getEmail());
 	}
+	
+	
 function onFailure(error) {
 		console.log(error);
 	}
+	
+	
 function renderButton() {
 		gapi.signin2.render('my-signin2', {
 			'scope': 'profile email',
@@ -36,12 +43,14 @@ function renderButton() {
 			'onfailure': onFailure
 			});
 	}
-	
 
+
+
+	
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
   }
-
+ 
