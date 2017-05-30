3723e38438c8f652ba39619927327e730b1987b5
annaddssdd <annaddssdd@gmail.com>
2017-05-30 17:47:52 +1200

    Update poo.js

--- poo.js
+++ poo.js
@@ -1,6 +1,6 @@
 var clicks = 0;
-//var signInSuccess = false;
-//var signInName = "";
+var signInSuccess = false;
+var signInName = "";
 function onClick() {
 		if (clicks === 0) {
 		document.getElementById("demo").innerHTML = "go back to your family"; 
@@ -21,7 +21,7 @@ function onClick() {
 
 
 function onSuccess(googleUser) {
-	//signInSuccess = true;
+	signInSuccess = true;
 	var profile = googleUser.getBasicProfile();
 	var name = profile.getName();
 	var firstName = profile.getGivenName();
@@ -30,7 +30,7 @@ function onSuccess(googleUser) {
 	console.log('Logged in as: ' + name);	
 	console.log('ID: ' + profile.getId());
 	console.log('Email: ' + profile.getEmail());
-	//document.getElementById("demo2").innerHTML = "Find out who's eating out your juicy " + firstName + " Mc" + lastName + " combo!";
+	document.getElementById("demo2").innerHTML = "Find out who's eating out your juicy " + firstName + " Mc" + lastName + " combo!";
 }
 
 function loadGirlfriendHTML(profileName) {
