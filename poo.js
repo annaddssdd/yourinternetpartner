531b011ea1d2f68e72b0fd9b193eaf31d3ef8e87
annaddssdd <annaddssdd@gmail.com>
2017-05-30 17:56:47 +1200

    Update poo.js

--- poo.js
+++ poo.js
@@ -75,6 +75,7 @@ function renderButton() {
 
 
 function signOut() {
+	signInSuccess = false;
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
