e7bf82cd8508aed1d4cf8fb1676b35650094dee0
annaddssdd <annaddssdd@gmail.com>
2017-06-03 15:37:30 +1200

    Update poo.js

--- poo.js
+++ poo.js
@@ -128,7 +128,7 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
-		document.getElementById("demo2").innerHTML = "CHECK OUT YOUR GIRLFRIEND NOW!";
+		document.getElementById("demo2").innerHTML = "SIGN IN AND WATCH THIS SPACE....";
 		document.getElementById("congratz").innerHTML = "";
 		document.getElementById("clickAgain").innerHTML = "";
 		location.reload();
